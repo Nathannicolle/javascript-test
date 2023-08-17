a8565796ae76b23ed3752b1053ec619a3aff019e
Nathannicolle <nathan-nicolle@orange.fr>
2023-08-17 18:39:43 +0200

    ajout d'un cercle sur la carte
    (utile pour désigner le rayon d'action d'un professionnel par exemple)

--- test_4_osm_circle/script.js
+++ test_4_osm_circle/script.js
@@ -1,7 +1,21 @@
 window.addEventListener("load", () => {
+   // Initialisation de la carte
    let map = L.map('map').setView([49.18660241815089, -0.36225316822461645], 13);
+
+   // Ajout de la partie "crédits" (copyright OSM...)
    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
-      attribution: '© OpenStreetMap'
+      attribution: '© OpenStreetMap (test.fr)'
+   }).addTo(map);
+
+   // Ajout du marqueur de position
+   var marker = L.marker([49.18660241815089, -0.36225316822461645]).addTo(map);
+
+   // Ajout d'un cercle
+   var circle = L.circle([49.18660241815089, -0.36225316822461645], {
+      color: '#033C49',
+      fillColor: '#033C49',
+      fillOpacity: 0.5,
+      radius: 5000
    }).addTo(map);
 });
